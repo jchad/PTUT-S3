17590cb835f4a997919858833f7c0024724db1a5
maxime <maxime.bill@hotmail.fr>
2016-09-20 13:01:51 +0200

    etat actuel ?
    test

--- Ptut/Assets/MoveScript.cs
+++ Ptut/Assets/MoveScript.cs
@@ -1,22 +1,36 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class MoveScript : MonoBehaviour {
-    public Vector2 pos = new Vector2(0, 0);
-    public Vector2 speed = new Vector2(0.01f,0);
-    public float posX = 0;
-    public float posY = 0;
-    // Use this for initialization
-    void Start () {
-        pos = transform.position;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        posX = pos.x + speed.x;
-        posY = pos.y + speed.y;
-        pos = pos + new Vector2(posX, posY);
-        transform.position = pos;
-        Debug.Log(pos);
-	}
+/// <summary>
+/// Déplace l'objet
+/// </summary>
+public class MoveScript : MonoBehaviour
+{
+    // 1 - Designer variables
+
+    /// <summary>
+    /// Vitesse de déplacement
+    /// </summary>
+    public Vector2 speed = new Vector2(10,0);
+
+    /// <summary>
+    /// Direction
+    /// </summary>
+    public Vector2 direction = new Vector2(1, 0);
+
+    private Vector2 movement;
+
+    void Update()
+    {
+        // 2 - Calcul du mouvement
+        movement = new Vector2(
+          speed.x * direction.x,
+          speed.y * direction.y);
+    }
+
+    void FixedUpdate()
+    {
+        // Application du mouvement
+        GetComponent<Rigidbody2D>().velocity = movement;
+        Debug.Log(transform.position);
+    }
 }
