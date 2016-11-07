c56d40e4068f4f8cf6f99c5a8b6c2b81e36151d5
KasaiHitta <laurapacqueriaud@orange.fr>
2016-11-07 13:48:02 +0100

    ecran d'accueil premier jet

---
+++ Laura Ecran/Assets/Menu principal.js
@@ -0,0 +1,21 @@
+﻿
+function OnGUI (){
+
+	if(GUI.Button(Rect(Screen.width / 2 - 100, Screen.height / 2 - 150, 200, 50), "Jouer")){ 
+
+	Application.LoadLevel("Level 1"); 
+
+	}
+
+	if(GUI.Button(Rect(Screen.width / 2 - 100, Screen.height / 2 - 75, 200, 50), "Options")){
+
+
+	}
+
+	if(GUI.Button(Rect(Screen.width / 2 - 100, Screen.height / 2, 200, 50), "Quitter")){
+
+	  Application.Quit();
+
+	}
+
+}
