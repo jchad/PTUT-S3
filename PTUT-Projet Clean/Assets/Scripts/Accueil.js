d77f87949cf8f723cd45621d9a8edee2f8ec4b1a
Jordan CHADUIRON <jordan.chaduiron@etu.univ-lyon1.fr>
2016-11-24 15:42:51 +0100

    Lien page acceuil et login

--- PTUT-Projet Clean/Assets/Scripts/Accueil.js
+++ PTUT-Projet Clean/Assets/Scripts/Accueil.js
@@ -1,12 +1,10 @@
 ﻿function OnGUI (){
 
-    if(GUI.Button(Rect(Screen.width / 2 - 100, Screen.height / 2 - 150, 200, 50), "Jouer")){ 
-
-    Application.LoadLevel("Level 1"); 
-
+    if(GUI.Button(Rect(Screen.width / 2 - 100, Screen.height / 2 - 150, 200, 50), "Se connecter")){ 
+		Application.LoadLevel("Login");
     }
 
-    if(GUI.Button(Rect(Screen.width / 2 - 100, Screen.height / 2 - 75, 200, 50), "Options")){
+    if(GUI.Button(Rect(Screen.width / 2 - 100, Screen.height / 2 - 75, 200, 50), "S'inscrire")){
 
 
     }
