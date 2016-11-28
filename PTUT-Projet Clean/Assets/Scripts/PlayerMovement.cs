50e72eea8e3551d0d0485df755be9da3a24537d1
Zonthem <nico7.garaud@gmail.com>
2016-11-28 21:04:13 +0100

    saut qui marche

--- PTUT-Projet Clean/Assets/Scripts/PlayerMovement.cs
+++ PTUT-Projet Clean/Assets/Scripts/PlayerMovement.cs
@@ -4,35 +4,67 @@ using UnityEngine.Networking;
 public class PlayerMovement : NetworkBehaviour
 {
 	private float speedx;
-	private float speedy;
 
+	[SerializeField]
 	private float jumpSpeed = 5.0f;
-	private float veloY;
-	public float gravity = 20.0f;
-	public bool isGrounded = true;
 
 	private Rigidbody2D body;
-	public float maxSpeed = 5f;
 
+	[SerializeField]
+	private float maxSpeed = 5f;
+
+	[SerializeField]
+	private Transform groundPoint;
+
+	[SerializeField]
+	private float groundRadius;
+
+	[SerializeField]
+	private LayerMask sol;
+
+	private bool isGrounded;
+
+	private bool jump;
+
+// fin des variables
 	void Start()
 	{
 		body = GetComponent<Rigidbody2D> ();
 	}
 
-	void Update()
+	void FixedUpdate()
 	{
 		// Application du mouvement
 		speedx = Input.GetAxis("Horizontal");
 
+		isGrounded = IsGrounded ();
 
 		if (Input.GetButton ("Jump")) {
-			veloY = jumpSpeed;
-		} else {
-			veloY -= gravity * Time.deltaTime;
+			jump = true;
 		}
 
-		body.velocity = new Vector2(speedx * maxSpeed,  veloY);
+		movements ();
 	}
 
+	private void movements() {
+		
+		body.velocity = new Vector2(speedx * maxSpeed,  body.velocity.y);
+		if (isGrounded && jump) {
+			isGrounded = false;
+			body.AddForce (new Vector2 (0, jumpSpeed));
+		}
+	}
 
+	private bool IsGrounded() {
+		if (body.velocity.y <= 0) {
+			Collider2D[] colliders = Physics2D.OverlapCircleAll (groundPoint.position, groundRadius, sol);
+			for (int i = 0; i < colliders.Length; i++) {
+				if (colliders [i].gameObject != gameObject) {
+					jump = false;
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 }
