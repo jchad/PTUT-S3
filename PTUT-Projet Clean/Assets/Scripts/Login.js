44d7c9af83c200baa9ff78ec4ae3e936de2b615c
Jordan CHADUIRON <jordan.chaduiron@etu.univ-lyon1.fr>
2017-03-11 21:18:07 +0100

    Passage sur JSON pour le Login
    UnityScript Bae ou pas.

--- PTUT-Projet Clean/Assets/Scripts/Login.js
+++ PTUT-Projet Clean/Assets/Scripts/Login.js
@@ -1,10 +1,13 @@
-﻿private var formNick = ""; //this is the field where the player will put the name to login
+﻿import SimpleJSON;
+import JSONParse;
+private var formNick = ""; //this is the field where the player will put the name to login
 private var formPassword = ""; //this is his password
-static var infouser : Array;
+var infouser : User;
 var formText = ""; //this field is where the messages sent by PHP script will be in
 
 var urlip   = "http://checkip.dyndns.org/";
 static var pubIP = "";
+var xhr;
 
 
 var URL = "localhost/PTUT/login.php"; //change for your URL
@@ -29,6 +32,24 @@ function OnGUI() {
     }
 }
 
+public class User
+{
+	public var noJoueur : int;
+	public var pseudo : String;
+	public var dateInscription : Date;
+	public var tempsdeJeu : String;
+	public var niveau : int;
+
+	public function User(no : int, pse : String, dateI : Date, tempsJ : String, niv : int){
+		noJoueur = no;
+		pseudo = pse;
+		dateInscription = dateI;
+		tempsdeJeu = tempsJ;
+		niveau = niv;
+	}
+}
+
+
 function Login() {
     var form = new WWWForm(); //here you create a new form connection
     form.AddField( "myform_hash", hash ); //add your hash code to the field myform_hash, check that this variable name is the same as in PHP file
@@ -36,26 +57,36 @@ function Login() {
     form.AddField( "myform_pass", formPassword );
     var w = new WWW(URL, form); //here we create a var called 'w' and we sync with our URL and the form
     yield w; //we wait for the form to check the PHP file, so our game dont just hang
+
     if (w.error != null) {
         print(w.error); //if there is an error, tell us
     } else {
     	var test=w.text;
     	print(test);
-    	var temp=test.Split(";"[0]);
-    	infouser=temp;
-    	if(infouser[0]=="1"){
+    	data = JSON.Parse(test);
+    	var N = parseInt(data["Statut"]);
+    	print(data["pseudo"].Value);
+    	if(N==1){
+    		print("ok");
+    		print(data["pseudo"].Value);
+    		infouser = new User(parseInt(data["noJoueur"]),data["pseudo"].Value,data["dateInscription"].AsDate,data["tempsdeJeu"].Value,parseInt(data["niveau"]));
+    		print("ok");
     		CheckIP();
     		Debug.Log(pubIP);
-    		print(infouser[0]);
-    		print(infouser[1]);
-    		print(infouser[2]);
-    		Application.LoadLevel("Main");
+    		//Application.LoadLevel("Main");
     	}else{
-    		formText = w.text; //here we return the data our PHP told us
+    		formText = data["message"]; //here we r	eturn the data our PHP told us
         }
         print("Test ok");
         w.Dispose(); //clear our form in game
     }
+
+
+	/*request(readData);
+	Debug.Log(xhr.reponseText);
+	infouser = JSON.Parse(xhr.reponseText);*/
+
+
 
     formNick = ""; //just clean our variables
     formPassword = "";
@@ -69,3 +100,47 @@ function CheckIP(){
    //pubIP = pubIP.Substring(0,pubIP.IndexOf(“<"));
     Debug.Log(pubIP);
 }
+/*
+function getXMLHttpRequest() {
+	var xhr = null;
+	
+	if (Application.XMLHttpRequest || Application.ActiveXObject) {
+		if (Application.ActiveXObject) {
+			try {
+				xhr = new ActiveXObject("Msxml2.XMLHTTP");
+			} catch(e) {
+				xhr = new ActiveXObject("Microsoft.XMLHTTP");
+			}
+		} else {
+			xhr = new XMLHttpRequest(); 
+		}
+	} else {
+		alert("Votre navigateur ne supporte pas l'objet XMLHTTPRequest...");
+		return null;
+	}
+	
+	return xhr;
+}
+function request(callback) {
+	xhr = getXMLHttpRequest();
+	
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) {
+			callback(xhr.responseText);
+		}
+	};
+	
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	xhr.send("myform_hash="+hash+"&myform_nick="+formNick+"&myform_pass="+formPassword);
+}
+
+function readData(sData) {
+	// On peut maintenant traiter les données sans encombrer l'objet XHR.
+	if (sData == "OK") {
+		alert("C'est bon");
+	} else {
+		alert("Y'a eu un problème");
+	}
+}
+*/
