d05172391821f2923cf1e764142def1c7264bed3
Jordan CHADUIRON <jordan.chaduiron@etu.univ-lyon1.fr>
2017-03-22 23:03:49 +0100

    Finalisation inscription/connexion

--- PTUT-Projet Clean/Assets/Scripts/UserInfoDisplay.js
+++ PTUT-Projet Clean/Assets/Scripts/UserInfoDisplay.js
@@ -1,10 +1,16 @@
 ﻿#pragma strict
 
 function OnGUI() {
-    GUI.Label( Rect (100, 10, 80, 20), "Your nick:" ); //text with your nick
-    GUI.Label( Rect (100, 30, 80, 20), "Your pass:" );
+	GUI.Label( Rect (350, 50, 80, 20), "Numéro de joueur:" ); //text with your nick
+    GUI.Label( Rect (350, 70, 80, 20), "Pseudo:" ); //text with your nick
+    GUI.Label( Rect (350, 90, 80, 20), "Date d'inscription:" );
+    GUI.Label( Rect (350, 110, 80, 20), "Temps de Jeu:" ); //text with your nick
+    GUI.Label( Rect (350, 130, 80, 20), "Niveau:");
 
-   	//GUI.TextField ( Rect (190, 10, 100, 20), Login.infouser.pseudo ); 
-    //GUI.TextField ( Rect (190, 30, 100, 20), Login.infouser.dateInscription.ToString() ); 
+    GUI.Label ( Rect (500, 50, 100, 20), Login.infouser["noJoueur"].Value ); 
+   	GUI.Label ( Rect (500, 70, 100, 20), Login.infouser["pseudo"].Value ); 
+    GUI.Label ( Rect (500, 90, 100, 20), Login.infouser["dateInscription"].Value);
+    GUI.Label ( Rect (500, 110, 100, 20), Login.infouser["tempsdeJeu"].Value ); 
+    GUI.Label ( Rect (500, 130, 100, 20), Login.infouser["niveau"].Value); 
 
 }
